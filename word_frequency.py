a324b1e8490dff51ec2825b1a838e6a0bdf5d528
Alyshia Lozano Smith <alyshiapolk@gmail.com>
2020-05-20 17:59:53 -0400

    worked through steps 1,2,and 3. currently trying stuck on counting words

--- word_frequency.py
+++ word_frequency.py
@@ -5,10 +5,59 @@ STOP_WORDS = [
 ]
 
 
-def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+#remove punctuation
+#normalize all words to lowercase
+#remove "stop words" -- words used so frequently they are ignored
+#go through the file word by word and keep a count of how often each word is used
+#"""Read in `file` and print out the frequency of words in that file."""
 
+
+
+
+def remove_punctuation(text):
+    punct = "!\"#$%&'()*+,./:;<=>?@[\]^_`{|}~"
+    words_to_keep = ''
+    
+    for character in text:
+        if not character in punct:
+            words_to_keep += (character)
+            
+    return words_to_keep
+          
+def remove_stop_words(string, words_to_remove):
+    words_to_count = []
+    for word in string: 
+        if word != words_to_remove:
+            words_to_count.append(word)
+            return(words_to_count)
+
+def count_words(list):
+    word_count = [()]
+    for word in list:
+            if word in list:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
+    return word_count            
+    
+
+        
+        
+        
+
+
+def print_word_freq(file):
+     
+     file = open(file)
+     text = file.read()
+     text = text.lower()
+     words_to_keep = remove_punctuation(text)
+     words_to_count = remove_stop_words(words_to_keep, STOP_WORDS)
+     word_count = count_words(words_to_count)
+     print(word_count)
+     
+
+     file.close
 
 if __name__ == "__main__":
     import argparse
@@ -25,3 +74,5 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
+
